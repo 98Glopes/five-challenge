6d588e356f76c104682d1f6ccdf7d7755fe102f4
98Glopes <34631226+98Glopes@users.noreply.github.com>
2020-05-13 10:55:13 -0300

    Created FizzBuzz function

---
+++ five_challenge.py
@@ -0,0 +1,45 @@
+
+
+def FizzBuzz(collection):
+
+    result = ""
+    n_fizz = 0
+    n_buzz = 0
+    n_fizzbuzz = 0
+
+
+    for n in collection:
+
+        if (n % 3 == 0) and (n % 5 == 0):
+            
+            result += "FizzBuzz\n"
+            n_fizzbuzz += 1
+
+        elif n % 3 == 0:
+            
+            result += "Fizz\n"
+            n_fizz += 1
+
+        elif n % 5 == 0:
+
+            result += "Buzz\n"
+            n_buzz += 1
+
+        else:
+
+            result += "%i\n" % n
+
+    result += "...\n"
+    result += "Resumo\n"
+    result += "%i Fizz\n" % n_fizz
+    result += "%i Buzz\n" % n_buzz
+    result += "%i FizzBuzz\n" % n_fizzbuzz
+
+
+    return result
+
+if __name__ == "__main__":
+    
+
+    result = FizzBuzz(range(1, 1000**10**10))
+    print(result)
